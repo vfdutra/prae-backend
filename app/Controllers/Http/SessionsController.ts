b606652292b5e66ee2fd66d7cb9ef8e1adbdf4fd
Vinicius Dutra <vfdutra00@gmail.com>
2023-07-01 22:40:53 -0300

    alterações no login

--- app/Controllers/Http/SessionsController.ts
+++ app/Controllers/Http/SessionsController.ts
@@ -1,23 +1,18 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
-import Hash from '@ioc:Adonis/Core/Hash'
+// import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class SessionsController {
-  public async store ({ request, response }: HttpContextContract) {
-    const email = request.input('email')
-    const password = request.input('password')
+  public async store({ auth, request, response }: HttpContextContract) {
+    const email = request.input('email');
+    const password = request.input('password');
   
-    // Lookup user manually
-    const user = await User
-      .query()
-      .where('email', email)      
-      .firstOrFail()
-  
-    // Verify password
-    if (!(await Hash.verify(user.password, password))) {
-      return response.unauthorized('Invalid credentials')
-    } 
-    
-    return response.ok({user})    
-  }
+    try {
+      const token = await auth.use('api').attempt(email, password);
+      const user = await User.query().where('email', email).firstOrFail();
+      return response.ok({ token, user });
+    } catch (error) {
+      return response.unauthorized('Invalid credentials');
+    }
+  }  
 }
