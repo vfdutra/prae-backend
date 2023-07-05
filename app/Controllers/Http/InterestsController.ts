a694e8f7066f664ff024f22606158695305b2d52
Vinicius Dutra <vfdutra00@gmail.com>
2023-07-05 00:14:07 -0300

    Alteração rota que retorna todos os interesses

--- app/Controllers/Http/InterestsController.ts
+++ app/Controllers/Http/InterestsController.ts
@@ -2,6 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Interest from 'App/Models/Interest'
 import User from 'App/Models/User'
 import Book from 'App/Models/Book'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class InterestsController {
     public async create ({ request, response }: HttpContextContract) {
@@ -34,7 +35,20 @@ export default class InterestsController {
     }
 
     public async findAll ({ response }: HttpContextContract) {
-        const interests = await Interest.all()
+        //Return all interets with the data of the users and the books on query builder
+        const interests = await Database.query()
+                                        .select('interests.id',
+                                                'interests.status',
+                                                'users.name as user_name', 
+                                                'books.title as book_title',
+                                                'books.author as book_author',
+                                                'books.category as book_category',
+                                                'books.quantity as book_quantity',
+                                                'books.cover')
+                                        .from('interests')
+                                        .innerJoin('users', 'interests.user_id', 'users.id')
+                                        .innerJoin('books', 'interests.book_id', 'books.id')
+
         return response.ok({ interests })
     }
 
